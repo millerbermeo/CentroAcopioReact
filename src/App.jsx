d93ad93cee7ce756f6000f12b299c0702f12c886
millerbermeo <millerbermeo30@gmail.com>
2023-07-02 11:56:45 -0500

    creacion de dist

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 
 function App() {
   return (
-    <Switch>
-      <Route path="/" element={<Dashboard />} />
+    <Routes>
+      <Route exact path="/" element={<Dashboard />} />
       <Route path="/login" element={<Login />} />
-    </Switch>
+    </Routes>
   );
 }
 
