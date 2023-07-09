438ef5359386e5bd2f7ec33714b580c9ba0c291f
millerbermeo <millerbermeo30@gmail.com>
2023-07-09 03:03:59 -0500

    objeto date

--- src/components/Navtop.jsx
+++ src/components/Navtop.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 function Navtop() {
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  const formattedDate = date.toLocaleString();
   return (
     <div className="w-[100%] bg-[#F7F9FD] h-20 flex justify-end shadow-lg fixed top-0 right-0">
-      <div className="flex items-center gap-2 pr-3">
+      <div className="flex items-center gap-2 pr-3 w-full justify-end">
+        <p className="absolute top-8 left-72">{formattedDate}</p>
         <img className="w-[25px]" src="notifications-sharp.svg" alt="" />
         <span className="border-l pl-2 text-gray-400">Miller Rivera</span>
         <Link to="/login">
--- src/pages/AllResiduos.jsx
+++ src/pages/AllResiduos.jsx
@@ -26,38 +26,37 @@ function AllResiduos() {
           <h1 className="text-[17px] text-[gray] mb-4">
             Aqui puedes ver todos los residuos guardados:
           </h1>
-          <table className="md:table-fixed w-full border border-gray-500">
-            <thead className="bg-[#00324D]">
+          <table className="md:table-fixed w-full">
+            <thead className="bg-[#6c79e0]">
               <tr className="h-12">
-                <th className="border border-gray-500 pl-2">id</th>
-                <th className="border border-white pl-2">Nombre</th>
-                <th className="border border-white pl-2">Tipo</th>
-                <th className="border border-white pl-2">Cantidad (Kg)</th>
-                <th className="border border-white pl-2">Descripcion</th>
-                <th className="border border-white pl-2">Deposito</th>
-                <th className="border border-gray-500 pl-2">Area</th>
+                <th className="w-12 border border-white text-center pl-1">
+                  id
+                </th>
+                <th className="w-36 border border-white">Nombre</th>
+                <th className="border border-white">Tipo</th>
+                <th className="w-24 border border-white">Cantidad (Kg)</th>
+                <th className="w-64 border border-white">Descripcion</th>
+                <th className="w-32 border border-white">Deposito</th>
+                <th className="pl-2 border border-white">Area</th>
               </tr>
             </thead>
             <tbody className="text-gray-600 text-base">
-              {data.map((item) => (
-                <tr key={item.id} className="h-9 border border-blue-500">
-                  <td className="border border-gray-500 pl-2">{item.id}</td>
-                  <td className="border border-gray-500 pl-2">
-                    {item.nombre_residuo}
-                  </td>
-                  <td className="border border-gray-500 pl-2">
-                    {item.tipo_residuo}
-                  </td>
-                  <td className="border border-gray-500 pl-2">
-                    {item.cantidad_residuo}
-                  </td>
-                  <td className="border border-gray-500 pl-2 overflow-hidden">
+              {data.map((item, index) => (
+                <tr
+                  key={item.id}
+                  className={
+                    index % 2 === 0 ? "bg-white h-9" : "bg-[#edecff] h-9"
+                  }
+                >
+                  <td className="pl-2">{item.id}</td>
+                  <td className="pl-2">{item.nombre_residuo}</td>
+                  <td className=" pl-2">{item.tipo_residuo}</td>
+                  <td className=" pl-2">{item.cantidad_residuo}</td>
+                  <td className="pl-2 overflow-hidden">
                     {item.descripcion_residuo}
                   </td>
-                  <td className="border border-gray-500 pl-2">
-                    {item.deposito}
-                  </td>
-                  <td className="border border-gray-500 pl-2">{item.area}</td>
+                  <td className="pl-2">{item.deposito}</td>
+                  <td className="pl-2">{item.area}</td>
                 </tr>
               ))}
             </tbody>
